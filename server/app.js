e03f7fbbbe8ee257a19735c202f5ab4c1e4ee8d2
hwh96 <hwinuratama@gmail.com>
2022-02-14 00:02:07 +0700

    deploy

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const tweetController = require('./controllers/twitter-controller');
-app.use(cors());
+
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.get('/', tweetController.getEwlonTweet);
